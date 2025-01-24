e6bf6581968531438a3db4a9112d969a9c78c1b6
Naoufal <mopsyro@duck.com>
2025-01-24 22:52:30 +0100

    implementar la operacion Patch en la entidad BankAccount

--- src/main/java/com/personalfinancetracker/controllers/BankAccountController.java
+++ src/main/java/com/personalfinancetracker/controllers/BankAccountController.java
@@ -2,19 +2,20 @@ package com.personalfinancetracker.controllers;
 
 import com.personalfinancetracker.domain.dto.BankAccountDto;
 import com.personalfinancetracker.domain.entities.BankAccountEntity;
+import com.personalfinancetracker.exceptions.ErrorResponse;
 import com.personalfinancetracker.mapper.Mapper;
 import com.personalfinancetracker.service.BankAccountService;
-import com.personalfinancetracker.service.UserService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @RestController
 public class BankAccountController {
@@ -24,32 +25,58 @@ public class BankAccountController {
     private final Mapper<BankAccountEntity, BankAccountDto> bankAccountMapper;
 
 
-
     @Autowired
-    public BankAccountController(Mapper<BankAccountEntity, BankAccountDto> bankAccountMapper,  BankAccountService bankAccountService, UserService userService) {
+    public BankAccountController(Mapper<BankAccountEntity, BankAccountDto> bankAccountMapper, BankAccountService bankAccountService) {
         this.bankAccountMapper = bankAccountMapper;
         this.bankAccountService = bankAccountService;
     }
 
     @GetMapping(path = "/v1/bank-accounts/{user-id}")
-    public List<BankAccountDto> getBankAccounts(@PathVariable("user-id") Long userId) {
-        List<BankAccountEntity> authors = bankAccountService.findAllByUserId(userId);
-        return authors.stream().map(bankAccountMapper::mapTo).collect(Collectors.toList());
+    public Page<BankAccountDto> getBankAccounts(Pageable pageable, @PathVariable("user-id") Long userId) {
+        Page<BankAccountEntity> bankAccountEntities = bankAccountService.findAllByUserId(pageable, userId);
+        return bankAccountEntities.map(bankAccountMapper::mapTo);
     }
 
     @PostMapping(path = "/v1/bank-accounts")
-    public ResponseEntity<BankAccountDto> createBankAccount(@Valid @RequestBody BankAccountDto bankAccountDto) {
+    public ResponseEntity<Object> createBankAccount(@Valid @RequestBody BankAccountDto bankAccountDto) {
         try {
             BankAccountEntity bankAccountEntity = bankAccountMapper.mapFrom(bankAccountDto);
             BankAccountEntity saveBankAccount = bankAccountService.create(bankAccountEntity);
-            BankAccountDto response = bankAccountMapper.mapTo(saveBankAccount);
-            if (response != null) {
+            if (saveBankAccount != null) {
+                BankAccountDto response = bankAccountMapper.mapTo(saveBankAccount);
                 return new ResponseEntity<>(response, HttpStatus.CREATED);
             } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("The user does not exists",HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @PatchMapping(path = "/v1/bank-accounts/{user-id}/{bank-account-id}")
+    public ResponseEntity<Object> partialUpdate(@RequestBody BankAccountDto bankAccountDto,
+                                                @PathVariable("user-id") Long userId, @PathVariable("bank-account-id") Long bankAccountId) {
+        ResponseEntity<Object> errorResponse = bankAccountDoesNotExistsException(userId, bankAccountId);
+        if (errorResponse != null) return errorResponse;
+        bankAccountService.fillBankAccountDtoWithUserAndDetails(bankAccountDto, userId, bankAccountId);
+        try {
+            BankAccountEntity bankAccountEntity = bankAccountMapper.mapFrom(bankAccountDto);
+            BankAccountEntity saveBankAccount = bankAccountService.partialUpdate(bankAccountEntity);
+            BankAccountDto response = bankAccountMapper.mapTo(saveBankAccount);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private ResponseEntity<Object> bankAccountDoesNotExistsException(Long userId, Long bankAccountId) {
+        if (!bankAccountService.isExists(userId, bankAccountId)) {
+            ErrorResponse errorResponse = ErrorResponse
+                    .builder()
+                    .message("Bank Account Does Not Exists")
+                    .build();
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
+        return null;
     }
 }
--- src/main/java/com/personalfinancetracker/repositories/BankAccountRepository.java
+++ src/main/java/com/personalfinancetracker/repositories/BankAccountRepository.java
@@ -1,13 +1,21 @@
 package com.personalfinancetracker.repositories;
 
 import com.personalfinancetracker.domain.entities.BankAccountEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BankAccountRepository extends CrudRepository<BankAccountEntity, Long> {
+public interface BankAccountRepository extends CrudRepository<BankAccountEntity, Long>,
+        PagingAndSortingRepository<BankAccountEntity, Long> {
 
-    @Query("SELECT a from BankAccountEntity a where a.userEntity.id = ?1")
-    Iterable<BankAccountEntity> findAllByUserId(Long userId);
+    @Query("SELECT a FROM BankAccountEntity a WHERE a.userEntity.id = :userId")
+    Page<BankAccountEntity> findAllByUserId(Pageable pageable,  @Param("userId") Long userId);
+
+    @Query("SELECT COUNT(a) > 0 FROM BankAccountEntity a WHERE a.userEntity.id = :userId AND a.id = :bankAccountId")
+    boolean existsById(@Param("userId") Long userId, @Param("bankAccountId") Long bankAccountId);
 }
