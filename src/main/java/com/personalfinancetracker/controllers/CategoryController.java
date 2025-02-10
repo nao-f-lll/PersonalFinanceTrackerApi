260d6941f6e31fb7d3290d45d310077dc8a2d56b
Naoufal <mopsyro@duck.com>
2025-02-10 17:51:57 +0100

    Implementar operaciones GET para recuperar categorías padre e hijos

--- src/main/java/com/personalfinancetracker/controllers/CategoryController.java
+++ src/main/java/com/personalfinancetracker/controllers/CategoryController.java
@@ -1,19 +1,16 @@
 package com.personalfinancetracker.controllers;
 
-import com.personalfinancetracker.domain.dto.BankAccountDto;
 import com.personalfinancetracker.domain.dto.CategoryDto;
-import com.personalfinancetracker.domain.entities.BankAccountEntity;
 import com.personalfinancetracker.domain.entities.CategoryEntity;
 import com.personalfinancetracker.mapper.Mapper;
-import com.personalfinancetracker.service.BankAccountService;
 import com.personalfinancetracker.service.CategoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,6 +28,18 @@ public class CategoryController {
     @GetMapping(path = "/v1/categories")
     public Page<CategoryDto> getCategories(Pageable pageable) {
         Page<CategoryEntity> categoryEntities = categoryService.findAll(pageable);
+        return categoryEntities.map(categoryMapper::mapTo);
+    }
+
+    @GetMapping(path = "/v1/categories/parents")
+    public Page<CategoryDto> getParentCategories(Pageable pageable) {
+        Page<CategoryEntity> categoryEntities = categoryService.findParentCategories(pageable);
+        return categoryEntities.map(categoryMapper::mapTo);
+    }
+
+    @GetMapping(path = "/v1/categories/{parent-id}/subcategories")
+    public Page<CategoryDto> getSubCategoriesByParentName(Pageable pageable, @PathVariable("parent-id") Long parentId) {
+        Page<CategoryEntity> categoryEntities = categoryService.findSubCategoriesByParentId(pageable, parentId);
         return categoryEntities.map(categoryMapper::mapTo);
     }
 
--- src/main/java/com/personalfinancetracker/service/CategoryService.java
+++ src/main/java/com/personalfinancetracker/service/CategoryService.java
@@ -5,7 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public interface CategoryService {
     Page<CategoryEntity> findAll(Pageable pageable);
+
+    Page<CategoryEntity> findParentCategories(Pageable pageable);
+
+    Page<CategoryEntity> findSubCategoriesByParentId(Pageable pageable, Long parentId);
+
+    Optional<CategoryEntity> findOne(Long id);
 }
